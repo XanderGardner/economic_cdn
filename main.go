cad18b4bb9c3abb2c85bf782b3ec941e3c7b03dc
Xander Gardner <xandergardner7@gmail.com>
2023-12-12 23:23:17 -0500

    main

--- main.go
+++ main.go
@@ -11,15 +11,15 @@ func main() {
 	// Create a new MessageReceiver instance
 	messageReceiver := classes.NewMessageReceiver(8080)
 
-	// Start listening for incoming messages
-	if err := messageReceiver.StartListening(); err != nil {
-		fmt.Println(err)
-	}
+	// Start listening for incoming messages concurrently
+	go func() {
+		if err := messageReceiver.StartListening(); err != nil {
+			fmt.Println(err)
+		}
+	}()
 
 	// Create a new MessageSender instance
 	messageSender := classes.NewMessageSender("http://localhost:8080")
-
-	
 
 	// Message to be sent
 	message := "Hello, this is a test message!"
@@ -28,4 +28,10 @@ func main() {
 	if err := messageSender.SendMessage(message); err != nil {
 		fmt.Println(err)
 	}
+
+	// Sleep for a while to allow the receiver to process the message
+	// This is a simplistic approach, and you may need more sophisticated synchronization
+	// mechanisms in a real-world scenario.
+	// For example, you might use channels to signal the completion of message processing.
+	select {}
 }
