ddb81ac3b05a559ac00340c2e09be17080335ba8
Xander Gardner <xandergardner7@gmail.com>
2023-12-12 21:52:14 -0500

    folder structure

---
+++ classes/server_level1.go
@@ -0,0 +1,36 @@
+package messagereceiver
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+// MessageReceiver is a simple class for receiving messages on a specific port.
+type MessageReceiver struct {
+	Port int
+}
+
+// NewMessageReceiver creates a new instance of MessageReceiver with the given port.
+func NewMessageReceiver(port int) *MessageReceiver {
+	return &MessageReceiver{Port: port}
+}
+
+// StartListening starts the message receiver and listens for incoming messages.
+func (mr *MessageReceiver) StartListening() error {
+	// Simple HTTP handler function
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Printf("Received message: %s\n", body)
+	}
+
+	// Start the server on the specified port
+	http.HandleFunc("/", handler)
+	fmt.Printf("Server listening on :%d\n", mr.Port)
+	return http.ListenAndServe(fmt.Sprintf(":%d", mr.Port), nil)
+}
--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"main/messagereceiver"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	fmt.Println("Hello, World!")
+
+	// Create a new MessageReceiver instance
+	messageReceiver := NewMessageReceiver(8080)
+
+	// Start listening for incoming messages
+	if err := messageReceiver.StartListening(); err != nil {
+		fmt.Println(err)
+	}
+
+	// Create a new MessageSender instance
+	messageSender := NewMessageSender("http://localhost:8080")
+
+	
+
+	// Message to be sent
+	message := "Hello, this is a test message!"
+
+	// Send the message using the MessageSender
+	if err := messageSender.SendMessage(message); err != nil {
+		fmt.Println(err)
+	}
 }
