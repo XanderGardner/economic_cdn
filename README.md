773cfb7b87099d141438837134d9713083d8a0a5
Xander Gardner <xandergardner7@gmail.com>
2023-12-13 22:56:14 -0500

    users send requests on randome seconds

--- README.md
+++ README.md
@@ -16,7 +16,17 @@ go run server_level1/main.go receive_port send_port
 ```
 run user
 ```
-go run user/main.go send_port
+go run user/main.go send_port text_file_path
+```
+
+# example run
+```
+go run origin_server/main.go 8088
+go run server_level2/main.go 8084 8088
+go run server_level1/main.go 8081 8084
+go run user/main.go 8081 ./user/books/bacteria_wiki.txt
+
+
 ```
 
 # run testing for hyperbolic cache
--- user/main.go
+++ user/main.go
@@ -7,7 +7,17 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"io/ioutil"
+	"unicode"
+	"time"
+	"math/rand"
 )
+
+
+
+
+
+//////// MessageSender (user)
 
 // MessageSender is a simple class for sending messages to a specific port.
 type MessageSender struct {
@@ -35,9 +45,50 @@ func (ms *MessageSender) SendMessage(message string) error {
 	return nil
 }
 
+
+
+
+//////// helper functions
+
+// Given a string, returns the strings with only characters in the alphabete and spaces
+func keepAlphabeticAndSpaces(input string) string {
+	var result []rune
+
+	for _, char := range input {
+		// Keep alphabetic characters and spaces
+		if unicode.IsLetter(char) || unicode.IsSpace(char) {
+			result = append(result, char)
+		}
+	}
+
+	return string(result)
+}
+
+
+
+// Pauses the process execution for a random duration around one second
+func randomPause() {
+	// Seed the random number generator with the current time
+	rand.Seed(time.Now().UnixNano())
+
+	// Generate a random duration between 0.5 and 1.5 seconds
+	minDuration := 500 * time.Millisecond
+	maxDuration := 1500 * time.Millisecond
+	randomDuration := time.Duration(rand.Int63n(int64(maxDuration-minDuration)) + int64(minDuration))
+
+	// Pause for the generated random duration
+	time.Sleep(randomDuration)
+}
+
+
+
+
+//////// main function
+
 func main() {
 	// Default port
 	defaultPort := 8080
+	message_text := "hello!"
 
 	// Check if a port is provided as a command-line argument
 	if len(os.Args) > 1 {
@@ -46,14 +97,44 @@ func main() {
 			defaultPort = port
 		}
 	}
+	
+	if len(os.Args) > 2 {
+		// Read the contents of the file
+		filePath := os.Args[2]
+		content, err := ioutil.ReadFile(filePath)
+		if err != nil {
+			fmt.Printf("Error reading file: %s\n", err)
+			return
+		}
+
+		// Convert the byte slice to a string
+		message_text = keepAlphabeticAndSpaces(string(content))
+
+	}
 
 	// Create an instance of MessageSender with the specified port
 	sender := NewMessageSender(defaultPort)
 
-	// Send a message
-	message := "Hello from user!"
-	err := sender.SendMessage(message)
-	if err != nil {
-		fmt.Printf("Error sending message: %s\n", err)
+	
+
+	// Iterate over the words and send request to the cdn
+	words := strings.Split(message_text, " ")
+	for _, word := range words {
+		
+		// Send the word to the cdn (request the value, which is the lenght of the string)
+		err := sender.SendMessage(word)
+		if err != nil {
+			fmt.Printf("Error sending message: %s\n", err)
+		}
+		
+		// Pause for about a second
+		randomPause()
+
+
 	}
+	// Send a message
+	
+
+
+	
 }
